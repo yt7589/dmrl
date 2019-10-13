2869b170e711a3c836f7b3f21123186ad5aba0db
Yan Tao <yt7589@qq.com>
2019-10-13 17:07:10 +0800

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-  print('DMRL v0.0.1 build 1')
+  print('DMRL v0.0.1 build 2')
   x = np.linspace(0, 10, 100)
   fig = plt.figure()
   plt.plot(x, np.sin(x), '-')
   plt.plot(x, np.cos(x), '--')
+  plt.show()
 
 if '__main__' == __name__:
   main()
