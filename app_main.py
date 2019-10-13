a538a2e53ba2051feb9fd3946453ed23a2f20a83
Yan Tao <yt7589@qq.com>
2019-10-13 17:05:56 +0800

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,7 +1,13 @@
 #
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
   print('DMRL v0.0.1 build 1')
+  x = np.linspace(0, 10, 100)
+  fig = plt.figure()
+  plt.plot(x, np.sin(x), '-')
+  plt.plot(x, np.cos(x), '--')
 
 if '__main__' == __name__:
   main()
