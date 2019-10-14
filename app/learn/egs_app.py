4386de0cb3cda11f1857913018282251ea8f6e08
yantao <yantao@arxandt.com>
2019-10-14 15:28:19 +0800

    change @tf.function to static class method

--- app/learn/egs_app.py
+++ app/learn/egs_app.py
@@ -6,8 +6,19 @@ class EgsApp(object):
     def __init__(self):
         self.name = 'app.EgsApp'
 
+    @staticmethod
+    @tf.function
+    def train_step(model, optimizer, images, labels, train_loss, train_accuracy):
+        with tf.GradientTape() as tape:
+            predictions = model(images)
+            loss = loss_object(labels, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        train_loss(loss)
+        train_accuracy(labels, predictions)
+
     def startup(self):
-        print('Expert Getting Start App')
+        print('Expert Getting Start App v0.0.1')
         mnist = tf.keras.datasets.mnist
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
         x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -24,15 +35,6 @@ class EgsApp(object):
         test_loss = tf.keras.metrics.Mean(name='test_loss')
         test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
         @tf.function
-        def train_step(images, labels):
-            with tf.GradientTape() as tape:
-                predictions = model(images)
-                loss = loss_object(labels, predictions)
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-            train_loss(loss)
-            train_accuracy(labels, predictions)
-        @tf.function
         def test_step(images, labels):
             predictions = model(images)
             t_loss = loss_object(labels, predictions)
@@ -41,7 +43,7 @@ class EgsApp(object):
         EPOCHS = 5
         for epoch in range(EPOCHS):
             for images, labels in train_ds:
-                train_step(images, labels)
+                EgsApp.train_step(model, optimizer, images, labels, train_loss, train_accuracy)
             for test_images, test_labels in test_ds:
                 test_step(test_images, test_labels)
             template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
